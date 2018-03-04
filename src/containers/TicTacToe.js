ec79e3fe7c3622aefb8a87d5c8d67b802ece831c
kizggerg <gregorygzik@gmail.com>
2018-03-04 13:03:25 -0800

    Added Relay and Graph.cool components

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Stage } from 'react-konva';
 import { Board, Squares } from '../styled/TicTacToe';
+import Relay from 'react-relay/classic';
 
 class TicTacToe extends Component {
     constructor(props)
@@ -158,4 +159,16 @@ class TicTacToe extends Component {
     }
 }
 
-export default TicTacToe;
+export default Relay.createContainer(
+    TicTacToe, {
+        fragments: {
+            viewer: () => Relay.QL`
+                fragment on Viewer {
+                    user {
+                        id
+                    }
+                }
+            `,
+        }
+    }
+);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,28 @@ import {Route, IndexRoute} from 'react-router';
 import Template from '../containers/Template';
 import TicTacToe from '../containers/TicTacToe';
 import Profile from '../containers/Profile';
+import Relay from 'react-relay/classic';
+
+const ViewerQueries = {
+    viewer: () => Relay.QL`query { viewer }`
+}
 
 const createRoutes = () => {
     return (
         <Route
             path='/'
-            component={Template}    
+            component={Template}
+            queries={ViewerQueries}    
         >
             <IndexRoute
                 component={TicTacToe}
+                queries={ViewerQueries}
             />
 
             <Route
                 path={'/profile'}
                 component={Profile}
+                queries={ViewerQueries}
             />
 
         </Route>
